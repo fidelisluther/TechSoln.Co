e0b4ce0d662fb7adcf281f9232e14c4e15c667e9
fidelisluther <lutherfidel@gmail.com>
2023-03-21 19:17:29 +0300

    commit

---
+++ layout/scripts/countdown.js
@@ -0,0 +1,30 @@
+// Set the date we're counting down to
+var countDownDate = new Date("April 10, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Calculate the days, hours, minutes, and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the days, hours, minutes, and seconds left
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
